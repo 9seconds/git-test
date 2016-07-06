00999c29c0006d28964ed048c371a954ad5c9d30
9seconds <nineseconds@yandex.ru>
2016-07-06 16:21:04 +0300

    Add more test for custom functions

--- test_gittest.py
+++ test_gittest.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
+from __future__ import unicode_literals
+
 import imp
 import os
 import os.path
@@ -26,10 +28,27 @@ class TestFunctions(object):
         {},
         {"lalala": 1},
         dict.fromkeys(map(str, range(10))),
-        {"long": "line\n\n\n\n\nline"}
+        {"long": "line\n\n\n\n\nline"},
+        {"юникод": "да"}
     ))
     def test_serialization(self, data):
         serialized = GITTEST.dict_to_base64(data)
 
         assert "\n" not in serialized
         assert GITTEST.base64_to_dict(serialized) == data
+
+    def test_tmpfile(self):
+        with GITTEST.tmpfile() as filename:
+            assert os.path.isfile(filename)
+            assert os.access(filename, os.R_OK | os.W_OK)
+
+        assert not os.path.exists(filename)
+
+    def test_unicode_filereader(self, tmpdir):
+        fileobj = tmpdir.join("filename")
+        fileobj.write_text("latin\nпривет", "utf-8")
+
+        reader = GITTEST.unicode_filereader(fileobj.open(encoding="utf-8"))
+
+        assert next(reader) == "latin\n"
+        assert next(reader) == "привет"
